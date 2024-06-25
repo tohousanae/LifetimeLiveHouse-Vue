8a8777da8673b2ed191517c2b529f094ae63a275
tohousanae <122202405+tohousanae@users.noreply.github.com>
2024-06-26 02:42:14 +0800

    0626 scss改boostrap的primary顏色成功

---
+++ src/components/bugershop/end.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- footer start -->
+    <div class="container">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <div class="col-md-4 d-flex align-items-center">
+                <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                    <svg class="bi" width="30" height="24">
+                        <use xlink:href="#bootstrap"></use>
+                    </svg>
+                </a>
+                <span class="text-muted">網頁僅供資展國際上課使用，不為商業用途。 </span>
+            </div>
+
+            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+                <li class="ms-3"><a class="text-muted" href="#"><i class="bi bi-facebook fs-3"></i></a></li>
+                <li class="ms-3"><a class="text-muted" href="#"><i class="bi bi-instagram fs-3"></i></a></li>
+                <li class="ms-3"><a class="text-muted" href="#"><i class="bi bi-youtube fs-3"></i></a></li>
+            </ul>
+        </footer>
+    </div>
+
+    <!-- footer end -->
+</template>
+
+<script setup>
+
+</script>
+
+<style></style>
