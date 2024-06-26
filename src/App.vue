2813b81325f538b99ca96730a37be6e41f1be545
tohousanae <122202405+tohousanae@users.noreply.github.com>
2024-06-26 13:50:07 +0800

    0626 完成首頁套版測試

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <script setup>
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import Navbar from './components/bugershop/Navbar.vue';
+import Home from './components/bugershop/Home.vue';
 import end from './components/bugershop/end.vue'
 </script>
 
 <template>
   <header>
-
     <!--主導覽列-->
     <Navbar></Navbar>
     <!--主導覽列-->
   </header>
+  <!--首頁區域-->
+  <home></home>
+  <!--首頁區域-->
 
   <!--footer區域-->
   <end></end>
   <!--footer區域-->
-
 </template>
 
 <style lang="scss">
