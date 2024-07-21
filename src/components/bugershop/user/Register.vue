fe04fb41452c30f2c063bcd44e07e18015841fe4
tohousanae <122202405+tohousanae@users.noreply.github.com>
2024-07-22 03:59:24 +0800

    0722 完成註冊api串接後端測試，並完成預設會員權限與預設信箱、手機驗證狀態功能

--- src/components/bugershop/user/Register.vue
+++ src/components/bugershop/user/Register.vue
@@ -1,60 +1,79 @@
 <template>
   <!--註冊表單模板：https://codepen.io/1bbnuuu/pen/dyagBQz-->
   <div class="form-center">
-    <div class="container">
-      <div class="card">
-        <form @submit.prevent="sendRegisterForm" class="row needs-validation" novalidate>
-          <h2 class="register">會員註冊</h2>
-          <input
-            type="email"
-            id="exampleFormControlInput1"
-            placeholder="電子信箱(Email)"
-            v-model="email"
-            required
-          />
+    <form
+      @submit.prevent="sendRegisterForm"
+      class="row col-12 col-md-4 d-flex needs-validation"
+      novalidate
+    >
+      <div class="col-12 text-start text-md-center mb-3">
+        <h2>會員註冊</h2>
+      </div>
+      <div class="col-12 mb-3">
+        <label for="validationCustom01" class="form-label">信箱</label>
+        <input
+          type="email"
+          class="form-control"
+          id="validationCustom01"
+          placeholder="name@example.com"
+          required
+        />
+        <div class="valid-feedback">Looks good!</div>
+        <div class="invalid-feedback">此為必填欄位</div>
+      </div>
+      <div class="col-12 mb-3">
+        <label for="validationCustom02" class="form-label">會員名稱</label>
+        <input
+          type="text"
+          class="form-control"
+          id="validationCustom02"
+          placeholder="請填寫會員名稱"
+          required
+        />
+        <div class="valid-feedback">Looks good!</div>
+        <div class="invalid-feedback">此為必填欄位</div>
+      </div>
+      <div class="col-12 mb-3">
+        <label for="validationCustomUsername" class="form-label">Username</label>
+        <div class="input-group has-validation">
           <input
             type="text"
-            placeholder="會員名稱(Username)"
-            autocomplete="username"
-            v-model="username"
+            class="form-control"
+            id="validationCustomUsername"
+            aria-describedby="inputGroupPrepend"
             required
           />
-          <input
-            type="password"
-            minlength="8"
-            v-model="password"
-            placeholder="輸入密碼(Password)"
-            autocomplete="password"
-            required
-          />
-          <input
-            type="password"
-            minlength="8"
-            id="confirmPassword"
-            placeholder="確認密碼(Confirm Password)"
-            required
-          />
-          <div class="row">
-            <div class="col">
-              <input
-                type="number"
-                id="phoneNumber"
-                placeholder="電話號碼(phone number)"
-                v-model="phoneNumber"
-                required
-              />
-            </div>
-            <div class="col">
-              <input type="number" id="validation" placeholder="簡訊驗證碼" required />
-            </div>
-          </div>
-
-          <button class="tombol-register" type="submit" id="registerButton" formnovalidate>
-            註冊
-          </button>
-        </form>
+          <div class="valid-feedback">Looks good!</div>
+          <div class="invalid-feedback">此為必填欄位</div>
+        </div>
       </div>
-    </div>
+      <div class="col-12 mb-3">
+        <label for="validationCustom03" class="form-label">City</label>
+        <input type="text" class="form-control" id="validationCustom03" required />
+        <div class="valid-feedback">Looks good!</div>
+        <div class="invalid-feedback">此為必填欄位</div>
+      </div>
+      <div class="col-12 mb-3">
+        <label for="validationCustom04" class="form-label">性別</label>
+        <select class="form-select" id="validationCustom04" required>
+          <option selected disabled value="">請選擇...</option>
+          <option>男</option>
+          <option>女</option>
+          <option>其他</option>
+        </select>
+        <div class="valid-feedback">Looks good!</div>
+        <div class="invalid-feedback">此為必填欄位</div>
+      </div>
+      <div class="col-12 mb-3">
+        <label for="validationCustom05" class="form-label">Zip</label>
+        <input type="text" class="form-control" id="validationCustom05" required />
+        <div class="valid-feedback">Looks good!</div>
+        <div class="invalid-feedback">此為必填欄位</div>
+      </div>
+      <div class="col-12 mb-3">
+        <button class="btn btn-primary" type="submit">送出</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -68,137 +87,6 @@
   justify-content: center;
   align-items: center;
 }
-
-body {
-  margin: 0;
-  padding: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-  font-family: 'Plus Jakarta Sans', sans-serif;
-  background-image: url(/public/photo/library-2.jpg);
-}
-
-.container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-}
-
-.card {
-  width: 50%;
-  background-color: #ffffff;
-  backdrop-filter: blur(100px);
-  padding: 20px;
-  border-radius: 8px;
-  border: 2px solid;
-  overflow: hidden;
-  align-items: center;
-  justify-content: center;
-}
-
-h2 {
-  text-align: left;
-  font-weight: 600;
-  font-size: 30px;
-  margin-top: 0;
-}
-
-.register {
-  text-align: center;
-  font-weight: 600;
-  margin-top: 0;
-}
-
-.register {
-  font-size: 30px;
-}
-
-form {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-}
-
-.gambar-depan {
-  border-radius: 6px;
-}
-
-input {
-  padding: 20px;
-  border: none;
-  border-radius: 5px;
-  background-color: transparent;
-  color: #ff8800;
-  transition: all 250ms;
-}
-
-input:focus {
-  outline: none;
-  border-color: #db8d34;
-  background-color: #eeeeee;
-}
-
-.card form a {
-  padding: 15px 20px;
-  background-color: rgba(17, 17, 17, 0.8);
-  cursor: pointer;
-  color: #ffffff;
-  text-align: center;
-  text-decoration: none;
-  font-weight: 600;
-}
-
-.tombol-register {
-  padding: 15px 20px;
-  background-color: rgba(17, 17, 17, 0.8);
-  cursor: pointer;
-  color: #ffffff;
-  text-align: center;
-  text-decoration: none;
-  font-weight: 600;
-  border: none;
-  outline: none;
-}
-
-.tombol-register:hover,
-a:hover {
-  background-color: rgba(54, 54, 54, 0.8);
-  color: #ffffff;
-}
-
-.switch a {
-  cursor: pointer;
-  color: rgb(0, 47, 255);
-  text-align: center;
-  text-decoration: none;
-  font-weight: 600;
-}
-
-.switch {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-  margin-top: 10px;
-}
-
-@media (max-width: 575px) {
-  .card {
-    width: 100%;
-    max-width: 300px;
-    max-height: 880px;
-  }
-  input:focus {
-    outline: none;
-    border-color: #ff9306;
-  }
-  .mydict div {
-    width: 100%;
-  }
-}
 </style>
 
 <script setup>
@@ -208,30 +96,44 @@ import { ref } from 'vue'
 const API_URL = `${import.meta.env.VITE_API_SPOTURL}/User`
 
 // 定義表單資料變數
-const email = ref('')
-const username = ref('')
-const phoneNumber = ref('')
-const password = ref('')
+let email = ref('').value
+let username = ref('').value
+let password = ref('').value
+let phoneNumber = ref('').value
+let sex = ref('').value
+let birthday = ref(new Date()).value
+let permission = ref(1000).value
+let phoneValidation = ref(false).value
+let emailValidation = ref(false).value
 
 // 送出註冊表單
 async function sendRegisterForm() {
-  console.log(email, username, phoneNumber, password)
+  // 前端部分密碼不進行哈希加密，而是以https來保護資料，後端再進行哈希加密，這可以防止攻擊者攔截網路請求看到哈希值後進行重放攻擊
+  // 參考資料(https://academy.binance.com/zt/articles/what-is-a-replay-attack)
+
   // Send a POST request
   await axios({
     method: 'post',
     url: `${API_URL}/register`,
     data: {
-      email: email,
       name: username,
+      email: email,
       phoneNumber: phoneNumber,
-      password: password
+      password: password,
+      sex: sex,
+      birthday: birthday,
+      permission: permission,
+      phoneValidation: phoneValidation,
+      emailValidation: emailValidation
     }
   })
     .then(function (response) {
       console.log(response)
+      alert(response.data)
     })
     .catch(function (error) {
       console.log(error)
+      alert(error.response.data)
     })
 }
 </script>
