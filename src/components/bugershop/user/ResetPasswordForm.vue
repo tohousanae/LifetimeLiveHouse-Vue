9caa8c2ed7b00368a27e7e5a770b008732d32acf
tohousanae <122202405+tohousanae@users.noreply.github.com>
2024-08-14 21:15:57 +0800

    0814 測試404頁面

--- src/components/bugershop/user/ResetPasswordForm.vue
+++ src/components/bugershop/user/ResetPasswordForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1 v-if="忘記密碼連結是否可用 == true">This is an ResetPwd page</h1>
+    <h1>This is an ResetPwd page</h1>
   </div>
 </template>
 
@@ -16,21 +16,16 @@
 
 <script setup>
 import axios from 'axios'
-import { ref } from 'vue'
 import { onMounted } from 'vue'
 
-// 一些變數
-let 忘記密碼連結是否可用 = ref(false).value
+const API_URL = `${import.meta.env.VITE_API_SPOTURL}/getforgetPwdtoken`
 
-const API_URL = `${import.meta.env.VITE_API_SPOTURL}/User`
-
-async function getWeatherForecast() {
+async function getResetPasswordToken() {
   await axios
     .get(`${API_URL}`)
     .then(function (response) {
       // handle success
       console.log(response.data)
-      忘記密碼連結是否可用 == true
     })
     .catch(function (error) {
       // handle error
