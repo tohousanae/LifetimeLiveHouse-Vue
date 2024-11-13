a0acc022c423bf486320c3f51de980754cc671df
tohousanae <122202405+tohousanae@users.noreply.github.com>
2024-11-13 22:48:45 +0800

    測試ssl

--- src/components/bugershop/user/ResetPasswordForm.vue
+++ src/components/bugershop/user/ResetPasswordForm.vue
@@ -17,6 +17,22 @@
 <script setup>
 import axios from 'axios'
 import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+// const route = useRoute()
+
+// router.beforeEach(async (to, from) => {
+//   if (
+//     // 检查用户是否已登录
+//     !isAuthenticated &&
+//     // ❗️ 避免无限重定向
+//     to.name !== 'Login'
+//   ) {
+//     // 将用户重定向到登录页面
+//     return { name: 'Login' }
+//   }
+// })
 
 const API_URL = `${import.meta.env.VITE_API_SPOTURL}/User/getforgetPwdtoken`
 
@@ -30,7 +46,7 @@ async function getResetPasswordToken() {
     .catch(function (error) {
       // handle error
       console.log(error)
-      // router.push({ path: '/404' })
+      router.push({ path: '/404' })
     })
 }
 onMounted(() => {
