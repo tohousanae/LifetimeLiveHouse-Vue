8a3defd3bc25efdff80df28d4a99ffb891ac993a
tohousanae <122202405+tohousanae@users.noreply.github.com>
2025-04-22 16:01:21 +0800

    導覽列預計作為新聞推播使用的按鈕暫時改名

--- src/components/bugershop/Navbar-page.vue
+++ src/components/bugershop/Navbar-page.vue
@@ -17,7 +17,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <RouterLink class="nav-link" to="/">Vocaloid情報</RouterLink>
+            <RouterLink class="nav-link" to="/">Vocaloid音樂/周邊/官方/世界計劃日台服情報</RouterLink>
           </li>
           <li class="nav-item">
             <RouterLink class="nav-link" to="/about">關於我們</RouterLink>
