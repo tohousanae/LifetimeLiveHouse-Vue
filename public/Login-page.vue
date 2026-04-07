40e5eeaa0d50aa351461077d4587a2ad41512c7d
tohousanae <122202405+tohousanae@users.noreply.github.com>
2026-04-07 16:53:39 +0800

    Update Login-page.vue

--- public/Login-page.vue
+++ public/Login-page.vue
@@ -1,43 +1,23 @@
 <template>
-  <div
-    class="modal fade"
-    id="userModal"
-    tabindex="-1"
-    aria-labelledby="userModalLabel"
-    aria-hidden="true"
-  >
+  <div class="modal fade" id="userModal" tabindex="-1" aria-labelledby="userModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="userModalLabel">登入會員</h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
           <p>立即登入，隨時收到獨家優惠</p>
-          <form @submit.prevent="submitForm" class="needs-validation" novalidate>
+          <form @submit.prevent="submitForm" class="needs-validation" :class="{ 'was-validated': isInvalid }"
+            novalidate>
             <div class="form-floating mb-3">
-              <input
-                v-model="email"
-                type="email"
-                class="form-control"
-                id="floatingInput"
-                placeholder="name@example.com"
-              />
+              <input v-model="email" type="email" class="form-control" id="floatingInput"
+                placeholder="name@example.com" />
               <label for="floatingInput">電子郵件</label>
             </div>
             <div class="form-floating">
-              <input
-                v-model="password"
-                type="password"
-                class="form-control"
-                id="floatingPassword"
-                placeholder="Password"
-              />
+              <input v-model="password" type="password" class="form-control" id="floatingPassword"
+                placeholder="Password" />
               <label for="floatingPassword">密碼</label>
             </div>
             <a href="/forgetpassword" class="float-end">忘記密碼</a>
@@ -48,13 +28,7 @@
       </div>
     </div>
   </div>
-  <div
-    class="modal fade"
-    id="cartModal"
-    tabindex="-1"
-    aria-labelledby="cartModalLabel"
-    aria-hidden="true"
-  >
+  <div class="modal fade" id="cartModal" tabindex="-1" aria-labelledby="cartModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-body">您的購物車是空的</div>
@@ -82,26 +56,30 @@ let password = ref('').value
 // let regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 
 async function submitForm() {
-  // 前端部分密碼不進行哈希加密，而是以https來保護資料，後端再進行哈希加密，這可以防止攻擊者攔截網路請求看到哈希值後進行重放攻擊
-  // 參考資料(https://academy.binance.com/zt/articles/what-is-a-replay-attack)
-  // ajax請求使用非同步方式，可以避免網頁在請求過程中無法操作的情況
-  // 登入後若身分為員工則直接跳轉到後台頁面，否則跳轉到前台頁面
-  await axios({
-    method: 'post',
-    url: `${API_URL}/login`,
-    withCredentials: true, //ajax請求有用到cookie時都要設定withCredentials: true
-    data: {
-      email: email,
-      password: password
-    }
-  })
-    .then(function (response) {
-      console.log(response)
-      alert(response.data)
-    })
-    .catch(function (error) {
-      console.log(error)
-      alert(error.response.data)
-    })
+  isInvalid.value = false
+  errorMsg.value = ''
+
+  try {
+    const response = await axios.post(
+      `${API_URL}/login`,
+      {
+        email: email.value,
+        password: password.value
+      },
+      { withCredentials: true }
+    )
+
+    console.log(response)
+    alert(response.data)
+
+    // 成功 → 清除錯誤狀態
+    isInvalid.value = false
+    errorMsg.value = ''
+  } catch (error) {
+    console.log(error)
+
+    isInvalid.value = true
+    errorMsg.value = error?.response?.data ?? '登入失敗'
+  }
 }
 </script>
