05774d44d6e00af569506b67f1e2fdec1ad4d447
tohousanae <122202405+tohousanae@users.noreply.github.com>
2026-05-20 23:16:19 +0800

    測試假ssl

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import viteBasicSslPlugin from "@vitejs/plugin-basic-ssl";
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +12,10 @@ export default defineConfig({
     host: true,
     https: true 
   },
-  plugins: [vue(), vueDevTools(),viteBasicSslPlugin()],
+  plugins: [vue(), 
+    vueDevTools(),
+    viteBasicSslPlugin(),
+    mkcert()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
