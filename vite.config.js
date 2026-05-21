fc57b92c55eebb9a797a43db13bb486eea9d20eb
tohousanae <122202405+tohousanae@users.noreply.github.com>
2026-05-21 12:36:40 +0800

    Revert "登入畫面導入多語系，正在測試回傳訊息是否有翻譯成多語系版"
    This reverts commit 233814a56c9d552249f2f0239b95d4a48f05e288.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import viteBasicSslPlugin from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
   { https: true ,
     host: true
   },
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(),viteBasicSslPlugin()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
