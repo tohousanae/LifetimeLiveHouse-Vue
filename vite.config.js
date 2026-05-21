7a5cf2155def04092385f99d9ef0c6509ce6902a
tohousanae <122202405+tohousanae@users.noreply.github.com>
2026-05-21 10:21:26 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,10 @@ import viteBasicSslPlugin from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { https: true },
+  server: 
+  { https: true ,
+    host: true
+  },
   plugins: [vue(), vueDevTools(),viteBasicSslPlugin()],
   resolve: {
     alias: {
