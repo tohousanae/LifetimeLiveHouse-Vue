26f1c3f99f8d4d3e58bfabd64950592084c03cd8
tohousanae <122202405+tohousanae@users.noreply.github.com>
2026-08-10 23:26:06 +0800

    複製sanae分支的內容到dev分支，藉此快速修復做錯的ui

---
+++ src/components/Mikushop/Others/NotFound.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="about">
+      <h1>404</h1>
+      <p>您要求的內容不存在</p>
+    </div>
+  </template>
--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import viteBasicSslPlugin from "@vitejs/plugin-basic-ssl";
-import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { 
-    host: true,
-    https: true 
-  },
-  plugins: [vue(), 
-    vueDevTools(),
-    viteBasicSslPlugin(),
-    mkcert()],
+  server: { https: true },
+  plugins: [vue(), vueDevTools(),viteBasicSslPlugin()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
